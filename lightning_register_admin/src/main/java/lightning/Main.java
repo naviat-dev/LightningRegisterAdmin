d655d771e1690fc8d2355a66752d2812878eee68
Israel Emmanuel <sriemmanuel787@outlook.com>
2024-08-02 17:08:25 -0700

    This will be useful when I connect MySQL

--- lightning_register_admin/src/main/java/lightning/Main.java
+++ lightning_register_admin/src/main/java/lightning/Main.java
@@ -1,11 +1,15 @@
 package lightning;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.io.*;
+
+import com.mysql.cj.jdbc.Driver;
 
 class Main {
     public static void main(String[] args) throws FileNotFoundException {
+        // We will need to have passwords so that unauthorised people cannot screw things up
         ArrayList<String> validPasswords = new ArrayList<String>();
         Scanner getPasswords = new Scanner(new File("validId.txt"));
         while (getPasswords.hasNextLine()) {
